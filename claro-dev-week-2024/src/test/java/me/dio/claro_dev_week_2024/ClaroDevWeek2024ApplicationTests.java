164455f9ea21206aeba87ccc94fec17a766365f4
YukiDevill <sthefanyfelix10@gmail.com>
2024-11-17 19:01:15 -0300

    first commit

---
+++ claro-dev-week-2024/src/test/java/me/dio/claro_dev_week_2024/ClaroDevWeek2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.claro_dev_week_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaroDevWeek2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
